6a065192ae7a821aa9516474a5137ffeeba69424
Sylwia <syladalis@gmail.com>
2024-01-30 15:53:53 +0100

    first table

--- My_Budget.py
+++ My_Budget.py
@@ -1,6 +1,17 @@
-import tabulate
 print('Monthly Budget')
 
+income = {
+          1: ["Przychód", int(input('Define your monthly income: '))],
+          2: ["ZUS", int(input('Define ZUS amount: '))],
+          3: ["Podatek", int(input('Define Tax amount: '))],
+          4: ["VAT", int(input('Define VAT amount: '))],
+          5: ["Księgowa", int(input('Define accountant costs: '))],
+          6: ["Dochód", "Here goes a sum of costs minus income value"],
+          }
+print("{:<10} {:<10} ".format('Nazwa', 'Kwota'))
 
+for key, value in income.items():
+    name, sum  = value
+    print("{:<10} {:<10}".format(name, sum))
 
 
